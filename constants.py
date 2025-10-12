4b9a0ff1d4386d6d11ffc78752b5af404cbf1a54
minhdq <minhdq1991@gmail.com>
2025-10-12 15:41:00 +0700

    update

---
+++ constants.py
@@ -0,0 +1,14 @@
+MODELS = {
+    "LR": {
+        "name": "Logistic Regression",
+        "path": "model/logistic_regression_pipeline.pkl",
+        "value": "LR",
+        "threshold": 0.6
+    },
+    "SVM": {
+        "name": "Support Vector Machine",
+        "path": "model/svm_pipeline.pkl",
+        "value": "SVM",
+        "threshold": 0.7
+    }
+}
--- login_detect.py
+++ login_detect.py
@@ -1,37 +1,59 @@
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 import joblib
-import numpy as np
+import os
 import pandas as pd
+from constants import MODELS
 
 app = Flask(__name__)
 
-# Load model khi khởi động server
-model = joblib.load("model/login_pipeline.pkl")
+# Function to extract time-based features
+def extract_time_features(x):
+    x['hour'] = x['Timestamp'].dt.hour
+    x['day_of_week'] = x['Timestamp'].dt.dayofweek
+    return x[['hour', 'day_of_week']]
+
+# Load model from environment variable
+load_dotenv(dotenv_path="config.env")
+model_type = os.getenv("MODEL_TYPE", "")
+if model_type not in MODELS:
+    raise ValueError(f"Model invalid: {model_type}")
+info = MODELS[model_type]
+print(f"Loading {info['name']} from {info['path']}")
+model = joblib.load(info["path"])
+threshold = info["threshold"]
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Nhận JSON từ client (Spring Boot gửi qua)
-    data = request.get_json()
-    print(f"Received data: {data}")
+    try:
+    # Request data to predict
+        data = request.get_json()
 
+        # Mapping data to DataFrame
+        columns = ['User ID', 'Timestamp', 'Login Status', 'IP Address', 'Device Type',
+            'Location', 'Session Duration', 'Failed Attempts', 'Behavioral Score']
+        df = pd.DataFrame([data['features']], columns=columns)
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'], errors='coerce')
+        # Predict
+        probs = model.predict_proba(df)[0][1]
+        prediction = int(probs >= threshold)
 
-    # Tạo DataFrame với đúng tên cột
-    columns = ['User ID', 'Timestamp', 'Login Status', 'IP Address', 'Device Type',
-           'Location', 'Session Duration', 'Failed Attempts', 'Behavioral Score']
-
-    df = pd.DataFrame([data['features']], columns=columns)
-    # Dự đoán
-    df = pd.DataFrame([data['features']], columns=columns)
-
-
-    prediction = model.predict(df)
-    probability = model.predict_proba(df)[0].tolist()
-
-    # Trả kết quả
-    return jsonify({
-        "prediction": int(prediction[0]),
-        "probability": probability
-    })
+        # Response
+        return jsonify({
+            "code": 200,
+            "message": "Success",
+            "data": {
+                "prediction": prediction,
+                "probability": float(probs),
+                "model": info['value']
+            }
+        }), 200
+    except Exception as e:
+        print(f"Has error: {e}")
+        return jsonify({
+            "code": 400,
+            "message": str(e)
+        }), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
